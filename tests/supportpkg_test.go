ecdbbc2c4075fa5537fa9b828b3aeab3890ed74e
Daniel Areste Hernandez <d.areste@f5.com>
2024-09-26 16:03:44 +0200

    feat: add integration tests

--- tests/supportpkg_test.go
+++ tests/supportpkg_test.go
@@ -7,18 +7,19 @@ import (
 	"testing"
 )
 
-func TestKubectlCommands(t *testing.T) {
-	// Execute `kubectl` command
-	cmd := exec.Command("kubectl", "nginx-supportpkg")
+func TestKubectlNginxSupportpkgVersion(t *testing.T) {
+	cmd := exec.Command("kubectl", "nginx-supportpkg", "-v")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-	_ = cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		t.Errorf("Failed to run 'kubectl nginx-supportpkg -v': %v", err)
+	}
 
-	// Compare the output with expected result
-	expectedOutput := "required flag(s)"
+	expectedOutput := "nginx-supportpkg - version"
 	actualOutput := stdout.String()
 	if !strings.Contains(actualOutput, expectedOutput) {
-		t.Errorf("Expected output should contain \"%s\" but got \"%s\"", expectedOutput, actualOutput)
+		t.Errorf("Expected output should contain '%s' but got '%s'", expectedOutput, actualOutput)
 	}
 }
