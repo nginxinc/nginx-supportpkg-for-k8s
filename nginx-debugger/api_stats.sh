d49821ff24c87645c77cf26a60f6666fd087664b
Madhu RAJAGOPAL <m.rajagopal@f5.com>
2025-06-09 12:39:51 +1200

    Fix: api_stats.sh improvements * Get latest API version from the API itself * Provide commandline option for port and help

--- nginx-debugger/api_stats.sh
+++ nginx-debugger/api_stats.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env bash
-for i in /api/8/processes /api/8/connections /api/8/slabs /api/8/http/requests /api/8/http/server_zones /api/8/http/location_zones /api/8/http/caches /api/8/http/upstreams /api/8/http/keyvals; do
-  echo "**** $i ****" ; 
-  curl -s "127.0.0.1:8080/$i" | jq .; 
-  echo ""; 
+
+# Parse command line options
+set -e
+set -o pipefail
+while getopts "p:v:h" opt; do
+  case $opt in
+    p) API_PORT="$OPTARG"
+    ;;
+    h) echo "Usage: $0 [-p port]"
+       exit 0
+    ;;
+    \?)  echo "Invalid option -$OPTARG" >&2
+    echo "Usage: $0 [-p port]"
+        exit 1
+    ;;
+  esac
+done
+
+if [ $OPTIND -eq 1 ]; then
+  echo "No options were passed, exiting ..."
+  echo "Usage: $(basename "$0") [-p port]"
+  exit 1
+fi
+
+if [ -z "${API_PORT}" ]; then
+  echo 'Missing -p arg' >&2
+  exit 1
+fi
+
+api_versions=($(curl http://127.0.0.1:$API_PORT/api/ | sed -e  's/\[//g' -e 's/\]//g' -e 's/\,/ /g'))
+API_VERSION=${api_versions[-1]}
+echo "API_VERSION: $API_VERSION"
+
+echo "**** /api/$API_VERSION/nginx ****" ;
+curl -s "127.0.0.1:$API_PORT/api/$API_VERSION/nginx" | jq .;
+echo "";
+
+for i in /api/$API_VERSION/processes /api/$API_VERSION/connections /api/$API_VERSION/slabs /api/$API_VERSION/http/requests /api/$API_VERSION/http/server_zones /api/$API_VERSION/http/location_zones /api/$API_VERSION/http/caches /api/$API_VERSION/http/upstreams /api/$API_VERSION/http/keyvals; do
+  echo "**** $i ****" ;
+  curl -s "127.0.0.1:$API_PORT/$i" | jq .;
+  echo "";
 done
