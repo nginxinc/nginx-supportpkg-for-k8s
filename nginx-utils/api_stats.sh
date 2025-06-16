75fc05c0ea09c814c0c9de9ff0f4d7fa3614c1e7
Madhu RAJAGOPAL <m.rajagopal@f5.com>
2025-06-16 15:30:19 +1200

    Fix: Output JSON raw for unformatted redirection to file

--- nginx-utils/api_stats.sh
+++ nginx-utils/api_stats.sh
@@ -28,16 +28,16 @@ if [ -z "${API_PORT}" ]; then
   exit 1
 fi
 
-api_versions=($(curl http://127.0.0.1:$API_PORT/api/ | sed -e  's/\[//g' -e 's/\]//g' -e 's/\,/ /g'))
+api_versions=($(curl -s http://127.0.0.1:$API_PORT/api/ | sed -e  's/\[//g' -e 's/\]//g' -e 's/\,/ /g'))
 API_VERSION=${api_versions[-1]}
 echo "API_VERSION: $API_VERSION"
 
 echo "**** /api/$API_VERSION/nginx ****" ;
-curl -s "127.0.0.1:$API_PORT/api/$API_VERSION/nginx" | jq .;
+curl -s "127.0.0.1:$API_PORT/api/$API_VERSION/nginx" | jq -r '.';
 echo "";
 
 for i in /api/$API_VERSION/processes /api/$API_VERSION/connections /api/$API_VERSION/slabs /api/$API_VERSION/http/requests /api/$API_VERSION/http/server_zones /api/$API_VERSION/http/location_zones /api/$API_VERSION/http/caches /api/$API_VERSION/http/upstreams /api/$API_VERSION/http/keyvals; do
   echo "**** $i ****" ;
-  curl -s "127.0.0.1:$API_PORT/$i" | jq .;
+  curl -s "127.0.0.1:$API_PORT/$i" | jq -r '.';
   echo "";
 done
